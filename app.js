a48584e36cf5e3a8d372bd0f56d4091b1116e1ce
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-11-17 22:22:36 +0100

    add eslint and prettier

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-"use strict";
+'use strict'
 {
   $(function() {
-    $("#datetimepicker4").datetimepicker({
-      format: "L"
-    });
-  });
+    $('#datetimepicker4').datetimepicker({ format: 'L' })
+  })
 
-  const form = document.getElementById("js-form");
+  const form = document.getElementById('js-form')
 
-  form.addEventListener("submit", e => {
-    event.preventDefault();
-    console.log(e.target.todo.value);
-    console.log(e.target.deadline.value);
-    console.log(e.target.importance.value);
-  });
+  form.addEventListener('submit', e => {
+    event.preventDefault()
+    console.log(e.target.todo.value)
+    console.log(e.target.deadline.value)
+    console.log(e.target.importance.value)
+  })
 }
