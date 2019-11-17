584a703fdb771c66711a68f01f1629134fc1f682
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2019-11-18 05:45:27 +0900

    third days previwe add task

--- app.js
+++ app.js
@@ -15,3 +15,72 @@
     console.log(e.target.importance.value);
   });
 }
+
+//Todo Class : Represents a Todolist
+class Todolist {
+  constructor(todo, deadline, importance){
+    this.todo = todo;
+    this.deadline = deadline;
+    this.importance = importance
+  }
+}
+
+// UI Class : Handle UI Tasks
+class UI {
+  static displayTasks(){
+    const Storedtasks = [
+      {
+        todo: 'clean floor',
+        deadline: 'tommorow',
+        importance: 'high'
+      },
+      {
+        todo: 'clean floor',
+        deadline: 'tommorow',
+        importance: 'high'
+      }
+    ]
+
+    const tasks = Storedtasks;
+
+    tasks.forEach((task) => UI.addTaskToList(task));
+
+  }
+
+  static addTaskToList(task){
+    const list = document.querySelector('#task-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML =`
+      <td>${task.title}</td>
+      <td>${task.deadline}</td>
+      <td>${task.importance}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete"></a></td>
+    `
+    list.appendChild(row)
+  }
+
+}
+
+// Store Class : Handles Storage
+
+// Event :Display Tasks
+
+document.addEventListener('DOMContentLoaded',UI.displayTasks);
+
+// Event : Add a Task
+
+document.querySelector('#js-form').addEventListener('submit',(e) =>{
+  //prevent actual submit
+  e.preventDefault();
+  //Get form value
+  const task = document.querySelector('#task').value;
+  const deadline = document.querySelector('#deadline').value;
+  // radio buttonの情報についてどう定義すればいいのか調べる
+  const importance = document.querySelector('#importance').value;
+
+  const todolist = new Todolist(task,deadline,importance);
+  
+  UI.addTaskToList(task);
+})
